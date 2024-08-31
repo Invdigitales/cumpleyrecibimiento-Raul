da13ff5069bfba2ed41d671cb91b0e6752d99e96
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-08-31 18:42:48 -0300

    final01

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.651867764341!2d-58.466708724882814!3d-34.63823685932094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc9808c15b821%3A0x4f9345018836fd61!2sCraigmhor!5e0!3m2!1ses-419!2sar!4v1724616339301!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3273.064763383404!2d-56.195629999999994!3d-34.87972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f801b04ee2d89%3A0x3647d866f4df3e5f!2sDr.%20Alfredo%20Garcia%20Morales%20y%20Av.%20Agraciada!5e0!3m2!1ses-419!2sar!4v1725140466605!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
